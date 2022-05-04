c474b2ffc1fb8c8f22e98c8c6176bc2381abef7e
bmsani <bmsani6@gmail.com>
2022-05-04 21:03:06 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import ManageInventory from './components/ManageInventory/ManageInventory';
+import NotFound from './components/NotFound/NotFound';
+import Blog from './components/Blog/Blog';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='manageInventory' element={<ManageInventory></ManageInventory>}> </Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
