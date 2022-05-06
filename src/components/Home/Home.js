00097f344c940c95c4537ac16eb55722a49388d7
bmsani <bmsani6@gmail.com>
2022-05-06 22:34:04 +0600

    banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import hikingGear from '../../hook/hikingGear';
 import SingleItem from '../SingleItem/SingleItem';
+import './Home.css';
 
 const Home = () => {
     const gears = hikingGear;
     const products = gears.slice(0,6)
 
     return (
-        <div className='container p-0 mb-5'>
+        <div>
+            <div className='banner'>
+            <div className='container banner-text'> 
+            <h1>Welcome to Mountain Rokers </h1>
+            <h5>Manage all you necessary hiking gear </h5>
+             </div>
+            </div>
+            <div className='container p-0 mb-5'>
             <div className='row m-0'>
             {
                 products.map(product => <SingleItem product={product} key={product.id}></SingleItem> )
@@ -16,6 +24,7 @@ const Home = () => {
             <div className='d-flex'>
             <button className='btn btn-dark fw-bold text-white w-25 mx-auto my-5'>Show all items</button>
             </div>
+        </div>
         </div>
     );
 };
